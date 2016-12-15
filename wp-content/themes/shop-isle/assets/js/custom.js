3c7a067ab7cbada9cb9d2719d09790fda005a905
nathaly <nathalysalgado.f@gmail.com>
2016-12-15 16:53:00 -0500

    flexslider

--- wp-content/themes/shop-isle/assets/js/custom.js
+++ wp-content/themes/shop-isle/assets/js/custom.js
@@ -272,7 +272,7 @@
 
 			// Build carousel
 			$(this).owlCarousel( {
-				navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+				navigationText: ['<i class="fa fa-angle-left">←</i>', '<i class="fa fa-angle-right"> →</i>'],
 				navigation: navigation,
 				pagination: pagination,
 				paginationSpeed: 400,
